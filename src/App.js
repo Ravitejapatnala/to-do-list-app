d6a9c0af910738a9c86943fb77625006ffb4cc02
akhil <akhil.ce08@gmail.com>
2024-01-04 15:48:09 +0530

    first commit and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App=()=> {
+
+  const [todo, setTodo]= useState("");
+  const[updatetodo, setUpdatetodo]= useState([]);
+  const[id, setId]= useState(1);
+  
+
+  function addTask(e){
+    e.preventDefault();
+    setUpdatetodo([...updatetodo, {id: id, title: todo}])
+    setTodo("");
+    setId(id+1);
+  }
+
+  function deleteTodo(deleteid){
+    setUpdatetodo(updatetodo.filter((item)=>(item.id!=deleteid)))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={addTask}>
+        <input type="text" placeholder="Enter your task" onChange={(e)=>setTodo(e.target.value)} value={todo}></input>
+        <button type="submit">ADD</button>
+      </form>
+
+      <ol>
+        {updatetodo.map((item)=>(
+          <li key={item.id}>{item.title} <button onClick={()=>(deleteTodo(item.id))}>Delete</button> </li>
+        ))}
+      </ol>
     </div>
   );
 }
